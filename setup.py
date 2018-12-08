74c2840707ba706a9d840ff531c1372e8761e407
delphi <cpp.create@gmail.com>
2018-12-08 23:20:24 +0200

    Fix package name
    Signed-off-by: delphi <cpp.create@gmail.com>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author_email='cpp.create@gmail.com',
     description='Yandex Weather API python module',
     long_description=LONG_DESCRIPTION,
-    py_modules=['aio_yandex_weather'],
+    py_modules=['yandex_weather_api'],
     include_package_data=True,
     platforms='any',
     classifiers=[
